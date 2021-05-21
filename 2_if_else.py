9c704f6726945b2d62697ebfaf69cfaa3f5521b5
harilal <harilal.poyil@eficode.com>
2021-05-21 21:22:36 +0300

    modifications in 1 and 2

--- 2_if_else.py
+++ 2_if_else.py
@@ -6,8 +6,7 @@ import random
 import re
 import sys
 
-if __name__ == '__main__':
-    n = int(input().strip())
+def solution_one(n):
     if n % 2 != 0:
         print('Weird')
     elif n in range(2, 6):
@@ -16,3 +15,12 @@ if __name__ == '__main__':
         print('Weird')
     elif n > 20:
         print('Not Weird')
+
+def solution_two(n):
+    check = {True: "Not Weird", False: "Weird"}
+    print(check[n%2==0 and (n in range(2,6) or n > 20)])
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    solution_one(n)
+    # solution_two(n)
--- 3_arithmetic_operators.py
+++ 3_arithmetic_operators.py
@@ -1,7 +1,13 @@
 #!/bin/python3
 
+def solution_one(a, b):
+    print(a + b, a - b, a * b, sep='\n')
+
+def solution_two(a, b):
+    print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    print(a + b, a - b, a * b, sep='\n')
-    # print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
+    solution_one(a, b)
+    #solution_two(a, b)
