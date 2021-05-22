6a8cda7aa0f760171f506cdbc516bf1d5f778346
harilal <harilal.poyil@eficode.com>
2021-05-22 21:01:25 +0300

    4 division

--- 4_division.py
+++ 4_division.py
@@ -1,13 +1,14 @@
 #!/bin/python3
 
 def solution_one(a, b):
-    print(a + b, a - b, a * b, sep='\n')
+    print(a//b, a/b, sep='\n')
 
 def solution_two(a, b):
-    print('{0} \n{1} \n{2}'.format((a + b), (a - b), (a * b)))
+    print("{0}\n{1}".format(a//b, a/b))
 
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
     solution_one(a, b)
     # solution_two(a, b)
+    
