543c6107424180c3d12a0219e06a1899c8a27d64
harilal <harilal.poyil@eficode.com>
2021-05-30 10:11:29 +0300

    13 tuples

---
+++ 13_basic_data_types_tuples.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
+
+    
