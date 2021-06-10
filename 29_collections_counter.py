b36ec9fa32d5230034a1c7d82655f0c9c04a6a0e
harilal <harilal.poyil@eficode.com>
2021-06-10 17:57:00 +0300

    collections counter

---
+++ 29_collections_counter.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+from collections import Counter
+
+def solution_one(x, counter, n):
+    total = 0
+    for _ in range(n):
+        size, price = map(int, input().split())
+        if counter[size]:
+            total += price
+            counter[size] -= 1
+    print(total)
+
+def solution_two(x, counter, n):
+    total = []
+    for _ in range(n):
+        size, price = map(int, input().split())
+        if size in counter and counter[size] > 0:
+            total.append(price)
+            counter.subtract(Counter([size]))
+    print(total)
+
+if __name__ == '__main__':
+    x = int(input())
+    counter = Counter(list(map(int, input().split())))
+    n = int(input())
