c2df24830c6264e1d9b4a290ae8b414dfec602aa
Harilal <7802150+HarilalOP@users.noreply.github.com>
2021-07-13 10:24:21 +0300

    errors and exceptions exceptions

---
+++ errors_and_exceptions_exceptions.py
@@ -0,0 +1,11 @@
+#!bin/python3
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        try:
+            a, b = map(int, input().split())
+            print(a // b)
+        except ZeroDivisionError as e:
+            print('Error Code:', e)
+        except ValueError as e:
+            print('Error Code:', e)
