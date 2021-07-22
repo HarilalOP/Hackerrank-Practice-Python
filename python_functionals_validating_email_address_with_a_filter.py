0497eaaccdfe324845a000dd50b07c9757663f9d
Harilal <7802150+HarilalOP@users.noreply.github.com>
2021-07-22 11:45:00 +0300

    python functionals validating email address with a filter

---
+++ python_functionals_validating_email_address_with_a_filter.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import re
+
+def fun(s):
+    regex = '^[\\w-]+@[0-9a-zA-Z]+\\.[a-z]{1,3}$'
+    return True if re.match(regex, s) else False
+    
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+    filtered_emails = filter_mail(emails)
+    filtered_emails.sort()
+    print(filtered_emails)
